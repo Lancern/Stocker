7fceab6340a97a198b175a93df8f18eabfdf6f42
Lancern <msrlancern@126.com>
2019-06-07 15:25:20 +0800

    [ref] Remove redundant usings

--- Common/HBase/Implementations/DefaultHBaseClient.cs
+++ Common/HBase/Implementations/DefaultHBaseClient.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Stocker.HBase.Serialization;
 using Stocker.Util;
 
